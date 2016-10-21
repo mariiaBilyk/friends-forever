f267e287846bcac3e654ff079aa38493628cef02
mariiaBilyk <bilyk.mariia@gmail.com>
2016-10-21 18:50:15 +0300

    Move solution

---
+++ Sourses/FriendsForever.API/Controllers/FriendForeverController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace FriendsForever.API.Controllers
+{
+    [Authorize]
+    public class FriendForeverController : ApiController
+    {
+        public IHttpActionResult Get () {
+            return Ok();
+        }
+    }
+}
